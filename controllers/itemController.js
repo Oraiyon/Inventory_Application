d06a3dedc8b1d6c39addcfe8052fabd3565d2e95
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-03-23 11:03:25 +0800

    Implemented item controller & pages. Item update & delete functions.

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -22,7 +22,7 @@ exports.item_create_get = asyncHandler(async (req, res, next) => {
 });
 
 exports.item_create_post = [
-  body("name", "Name must be specified").trim().isLength({ min: 10 }).escape(),
+  body("name", "Name must be specified").trim().isLength({ min: 1 }).escape(),
   body("description", "Description must be specified").trim().isLength({ min: 1 }).escape(),
   body("category", "No category selected").isLength({ min: 1 }).escape(),
   body("in_stock").escape(),
@@ -45,7 +45,69 @@ exports.item_create_post = [
       return;
     } else {
       await item.save();
-      res.redirect("/catalog/items");
+      res.redirect(item.url);
+    }
+  })
+];
+
+exports.item_details_get = asyncHandler(async (req, res, next) => {
+  const item = await Item.findById(req.params.id).populate("category").exec();
+  res.render("item_details", {
+    title: "Item Details",
+    item: item
+  });
+});
+
+exports.item_delete_get = asyncHandler(async (req, res, next) => {
+  const item = await Item.findById(req.params.id).populate("category").exec();
+  res.render("item_delete", {
+    title: "Item Delete",
+    item: item
+  });
+});
+
+exports.item_delete_post = asyncHandler(async (req, res, next) => {
+  await Item.findByIdAndDelete(req.body.item_id);
+  res.redirect("/catalog/items");
+});
+
+exports.item_update_get = asyncHandler(async (req, res, next) => {
+  const [item, allCategories] = await Promise.all([
+    Item.findById(req.params.id).exec(),
+    Category.find().exec()
+  ]);
+  res.render("item_create", {
+    title: "Update Item",
+    categories: allCategories,
+    item: item,
+    errors: ""
+  });
+});
+
+exports.item_update_post = [
+  body("name", "Name must be specified").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description must be specified").trim().isLength({ min: 1 }).escape(),
+  body("category", "No category selected").isLength({ min: 1 }).escape(),
+  body("in_stock").escape(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const item = await Item.findById(req.params.id).exec();
+    if (!errors.isEmpty()) {
+      const allCategories = await Category.find().exec();
+      res.render("item_create", {
+        title: "Update Item",
+        categories: allCategories,
+        item: item,
+        errors: errors.array()
+      });
+      return;
+    } else {
+      item.name = req.body.name;
+      item.description = req.body.description;
+      item.category = req.body.category;
+      item.in_stock = req.body.in_stock;
+      await item.save();
+      res.redirect(item.url);
     }
   })
 ];
--- routes/catalog.js
+++ routes/catalog.js
@@ -32,4 +32,14 @@ router.get("/item/create", item_controller.item_create_get);
 
 router.post("/item/create", item_controller.item_create_post);
 
+router.get("/item/:id", item_controller.item_details_get);
+
+router.get("/item/:id/delete", item_controller.item_delete_get);
+
+router.post("/item/:id/delete", item_controller.item_delete_post);
+
+router.get("/item/:id/update", item_controller.item_update_get);
+
+router.post("/item/:id/update", item_controller.item_update_post);
+
 module.exports = router;
