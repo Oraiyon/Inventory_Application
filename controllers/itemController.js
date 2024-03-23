beeced05ceb95714e36e70d552f54026a9da032b
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-03-23 14:12:12 +0800

    Implemented multer & added file input to item_create.

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -2,6 +2,9 @@ const Item = require("../models/item");
 const Category = require("../models/category");
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
+const multer = require("multer");
+// dest starts from root
+const upload = multer({ dest: "./public/uploads/" });
 
 exports.item_list = asyncHandler(async (req, res, next) => {
   const allItems = await Item.find().populate("category").exec();
@@ -22,11 +25,14 @@ exports.item_create_get = asyncHandler(async (req, res, next) => {
 });
 
 exports.item_create_post = [
+  // upload goes first
+  upload.single("item_img"),
   body("name", "Name must be specified").trim().isLength({ min: 1 }).escape(),
   body("description", "Description must be specified").trim().isLength({ min: 1 }).escape(),
   body("category", "No category selected").isLength({ min: 1 }).escape(),
   body("in_stock").escape(),
   asyncHandler(async (req, res, next) => {
+    console.log(req.file);
     const errors = validationResult(req);
     const item = new Item({
       name: req.body.name,
